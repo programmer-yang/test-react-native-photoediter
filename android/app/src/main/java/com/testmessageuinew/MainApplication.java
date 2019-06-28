cfc0980d88c8034e84c7cf917f9b467e3038123a
yang <392539259@qq.com>
2019-06-28 15:58:01 +0800

    更新rn-lm-photoediter版本

--- android/app/src/main/java/com/testmessageuinew/MainApplication.java
+++ android/app/src/main/java/com/testmessageuinew/MainApplication.java
@@ -3,6 +3,7 @@ package com.testmessageuinew;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.iwhalecloud.picedit.RNPicEditPackage;
 import fr.snapp.imagebase64.RNImgToBase64Package;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPicEditPackage(),
             new RNImgToBase64Package()
       );
     }
